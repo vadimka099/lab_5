ac99e849c98269727276fd529ce6c8de43562bcd
vadimka099 <47887170+vadimka099@users.noreply.github.com>
2019-04-26 13:29:11 +0300

    Add files via upload

---
+++ Complex.cpp
@@ -0,0 +1,57 @@
+#include "Complex.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+double Complex::getRe() const {
+	return Re;
+}
+
+double Complex::getIm() const {
+	return Im;
+}
+
+Complex::Complex(double re, double im) : Re(re), Im(im) {}
+
+Complex &Complex::operator=(Complex const &rhs) {
+	cout << "After overloaded = is called." << endl;
+	Re = rhs.Re;
+	Im = rhs.Im;
+	return *this;
+}
+
+Complex &Complex::operator++() {
+	cout << "After overloaded ++ operator is called." << endl;
+	Re++;
+	Im++;
+	return *this;
+}
+
+Complex &Complex::operator--(int) {
+	cout << "After overloaded -- is called." << endl;
+	Re--;
+	Im--;
+	return *this;
+}
+
+bool Complex::operator==(Complex const &rhs) {
+	return Re == rhs.Re && Im == rhs.Im;
+}
+
+bool Complex::operator!=(Complex const &rhs) {
+	return !(Re == rhs.Re & Im == rhs.Im);
+}
+
+std::ostream &operator<<(std::ostream &os, const Complex &complex) {
+	os << "Re: " << complex.Re << " Im: " << complex.Im << endl;
+	return os;
+}
+
+std::istream &operator>>(std::istream &is, Complex &complex) {
+	cout << "Type in real part: ";
+	is >> complex.Re;
+	cout << "Type in imaginary part: ";
+	is >> complex.Im;
+	return is;
+}
---
+++ Vector.h
@@ -0,0 +1,15 @@
+#include <ostream>
+
+class Vector {
+private:
+	double x;
+	double y;
+public:
+	Vector(double x = 0, double y = 0);
+	Vector &operator=(Vector const &rhs);
+	bool operator==(Vector const &rhs);
+	bool operator!=(Vector const &rhs);
+
+	friend std::ostream &operator<<(std::ostream &os, const Vector &vector);
+	friend std::istream &operator>>(std::istream &is, Vector &vector);
+};
